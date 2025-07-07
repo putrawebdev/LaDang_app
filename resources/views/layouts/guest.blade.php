3ae7f25fa34c099b059a4da57afc2bc867d5f3c9
Daka <zerosix554@gmail.com>
2025-07-07 22:59:18 +0700

    fourth commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,14 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Kelola Gudang | Log in</title>
         @include('layouts.style')
+        <link rel="stylesheet" href="{{ asset('daka-asset/css/daka.css') }}">
     </head>
     <body class="hold-transition login-page">
         <div class="login-box">
-            <div class="login-logo">
-                <a href=""><b>Kelola Gudang</b></a>
+            <div class="logo-login-div">
+                <h2 class="logo-login text-dark">Kelola Gudang</h2>
             </div>
             <!-- /.login-logo -->
-            <div class="card">
+            <div class="card ">
                 {{ $slot }}
             </div>
         </div>
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,32 +1,33 @@
 <div>
     <div class="card-body login-card-body">
+        
         <p class="login-box-msg">Log In your account</p>
 
         <form wire:submit="login">
             @csrf
-                <div class="form-group mb-3">
-                    <label for="email">Email</label>
-                    <input wire:model="email" type="email" class="form-control
-                    @error('email') is-invalid @enderror" id="email" name="email">
-                    @error('email')
-                        <small class="text-danger">
-                            {{ $message }}
-                        </small>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input wire:model="password" type="password" class="form-control
-                    @error('password') is-invalid @enderror" id="password" name="password">
-                    @error('password')
-                        <small class="text-danger">
-                            {{ $message }}
-                        </small>
-                    @enderror
-                </div>
-            <div class="row">
+            <div class="form-group mb-3">
+                <label for="email">Email</label>
+                <input wire:model="email" type="email" class="form-control
+                @error('email') is-invalid @enderror" id="email" name="email">
+                @error('email')
+                    <small class="text-danger">
+                        {{ $message }}
+                    </small>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input wire:model="password" type="password" class="form-control
+                @error('password') is-invalid @enderror" id="password" name="password">
+                @error('password')
+                    <small class="text-danger">
+                        {{ $message }}
+                    </small>
+                @enderror
+            </div>
+            <div class="row ">
                 <!-- /.col -->
-                <div class="col-4">
+                <div class="col-12">
                     <button type="submit" class="btn btn-primary btn-block">Sign In</button>
                 </div>
                 <!-- /.col -->
